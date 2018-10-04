f398df535bb677eb512249d9206ae7cb29c2b480
Shouvik Basu <babanomania@gmail.com>
2018-10-04 06:36:11 +0530

    working version

---
+++ index.js
@@ -0,0 +1,72 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
+'use strict';
+const Alexa = require('alexa-sdk');
+var request = require('request');
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
+const APP_ID = 'amzn1.ask.skill.c40b80b6-21e9-4117-ba87-c8a8808033fb';
+
+const WELCOME_MSG = "Welcome to Funny News On The Web, <break time='1s'/> did you hear the news published in ";
+const HELP_MESSAGE = 'You can say tell me a funny news, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+const handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetNewFactIntent');
+    },
+    'GetNewFactIntent': function () {
+        var scope = this;
+        request('https://www.reddit.com/r/nottheonion/hot.json?limit=10', function(error, response, body){
+            var resp = JSON.parse(body);
+            var news_index = Math.ceil(9 * Math.random());
+            
+            var newsTitle = resp.data.children[news_index].data.title;
+            var newsDomain = resp.data.children[news_index].data.domain;
+            var newsLink = resp.data.children[news_index].data.url;
+
+            var speechOutput = WELCOME_MSG + newsDomain + " that <break time='0.5s'/> " + newsTitle;
+            scope.emit(':tell', speechOutput)
+        })
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+};
+
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
